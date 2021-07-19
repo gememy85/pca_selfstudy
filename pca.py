80bd31b1c1a8dced4ceee9ef1314633cdf0bc495
gememy85 <gememy85@gmail.com>
2021-07-19 13:01:20 +0900

    update 0719

---
+++ pca.py
@@ -0,0 +1,14 @@
+#%%
+import pandas as pd
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+# %%
+df_wine
+# %%
+from sklearn.model_selection import train_test_split
+X, y = df_wine.iloc[:,1:].values, df_wine.iloc[:, 0].values
+X_train, X_text, y_train, y_test = train_test_split(X, y, text_size=0.3, stratify=y, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
